ff61458ece7a3944c56546dd475fe10d565d7a9d
vandosant <sjskender@gmail.com>
2016-03-24 10:38:02 -0600

    Use jsx in MyTitle

--- js/MyTitle.js
+++ js/MyTitle.js
@@ -1,14 +1,13 @@
 var React = require('react')
 
-var div = React.DOM.div
-var h1 = React.DOM.h1
-
 var MyTitle = React.createClass({
   render () {
     return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
+      <div>
+        <h1 style={{color: this.props.color}}>
+          {this.props.title}
+        </h1>
+      </div>
     )
   }
 })
