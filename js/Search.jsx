c591a6ba9613e1aeb41a3e4df3a085e6bd0f0ac0
vandosant <sjskender@gmail.com>
2016-03-24 13:23:01 -0600

    Create ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,19 +1,13 @@
 const React = require('react')
 const data = require('../public/data')
+const ShowCard = require('./ShowCard')
 
 const Search = () => {
   return (
     <div className='container'>
       <div className='shows'>
         {data.shows.map((show) => (
-          <div className='show-card'>
-            <img src={`public/img/posters/${show.poster}`} className='show-card-img'/>
-            <div className='show-card-text'>
-              <h3 className='show-card-title'>{show.title}</h3>
-              <h4 className='show-card-year'>({show.year})</h4>
-              <p className='show-card-description'>{show.description}</p>
-            </div>
-          </div>
+          <ShowCard show={show} />
         ))}
       </div>
     </div>
