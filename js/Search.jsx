035c025f6b37fb3c770d2052e7197fe109d1df96
vandosant <sjskender@gmail.com>
2016-03-24 14:31:58 -0600

    Allow updating of the search term

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,12 +3,21 @@ const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is my search'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='container'>
         <header className='header'>
           <h1 className='brand'>svideo</h1>
-          <input className='search-input' type='text' placeholder='Search'/>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'
+             onChange={this.handleSearchTermEvent}/>
         </header>
         <div className='shows'>
           {data.shows.map((show) => (
