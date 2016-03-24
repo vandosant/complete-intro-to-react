f536ec2700af9712245cccefcdbe0ae52709bacf
vandosant <sjskender@gmail.com>
2016-03-24 14:23:47 -0600

    Add search functionality and track state

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,16 +2,22 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => {
-  return (
-    <div className='container'>
-      <div className='shows'>
-        {data.shows.map((show) => (
-          <ShowCard {...show} key={show.imdbID} />
-        ))}
+const Search = React.createClass({
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input className='search-input' type='text' placeholder='Search'/>
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => (
+            <ShowCard {...show} key={show.imdbID}/>
+          ))}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+})
 
 module.exports = Search
