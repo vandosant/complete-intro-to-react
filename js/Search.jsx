7b36a74cf307e0611838e7d0c86f1f8558214c60
vandosant <sjskender@gmail.com>
2016-03-24 14:47:07 -0600

    Make search dynamic

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,7 @@ const ShowCard = require('./ShowCard')
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is my search'
+      searchTerm: ''
     }
   },
   handleSearchTermEvent (event) {
@@ -16,11 +16,11 @@ const Search = React.createClass({
       <div className='container'>
         <header className='header'>
           <h1 className='brand'>svideo</h1>
-          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search'
-             onChange={this.handleSearchTermEvent}/>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent}/>
         </header>
         <div className='shows'>
-          {data.shows.map((show) => (
+          {data.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+          ).map((show) => (
             <ShowCard {...show} key={show.imdbID}/>
           ))}
         </div>
