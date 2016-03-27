07145b39d4821cdd5fc94fa5051a9a4277ffc7af
vandosant <sjskender@gmail.com>
2016-03-26 18:20:43 -0600

    Move shows into Store

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,29 +5,18 @@ const Search = require('./Search')
 const Layout = require('./Layout')
 const Details = require('./Details')
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
-const { shows } = require('../public/data')
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
 
 const App = React.createClass({
-  assignShow (nextState, replace) {
-    const showArray = shows.filter((show) => show.imdbID === nextState.params.id)
-
-    if (showArray.length < 1) {
-      return replace('/')
-    }
-
-    Object.assign(nextState.params, showArray[0])
-    return nextState
-  },
   render () {
     return (
       <Provider store={store}>
         <Router history={hashHistory}>
           <Route path='/' component={Layout}>
             <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} shows={shows} />
-            <Route path='/details/:id' component={Details} onEnter={this.assignShow} />
+            <Route path='/search' component={Search} />
+            <Route path='/details/:id' component={Details} />
           </Route>
         </Router>
       </Provider>
